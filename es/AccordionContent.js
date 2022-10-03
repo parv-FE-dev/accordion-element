1e63cb24cf8cf1645e0b311dbf97bd7dc86d3372
Rachin Kapoor <rachin@moxie.xyz>
2022-10-03 05:12:50 -0400

    try webpack

--- es/AccordionContent.js
+++
@@ -1,8 +0,0 @@
-export default function AccordionContent(props) {
-  return /*#__PURE__*/React.createElement("section", {
-    className: " " + (props.className || '') + " plgAccordionContent"
-  }, /*#__PURE__*/React.createElement("div", {
-    className: "plgAccordionContainWrap"
-  }, props.children));
-}
-;
