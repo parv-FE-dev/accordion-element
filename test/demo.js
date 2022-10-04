5fa8abb3150e44eea6e20a4fe489f5f40ae4f0d3
Abujdnyaneshwari <84312485+Abujdnyaneshwari@users.noreply.github.com>
2022-10-04 14:24:36 +0530

    Default theme

--- test/demo.js
+++ test/demo.js
@@ -7,14 +7,27 @@ import {AccordionItem} from "../lib/react-accordians";
 
 render(
 <div style={{width:"100%", height:"100%"}}>
-  <HorizontalAccordion showButtonOnOpen="true">
+  <HorizontalAccordion className='horizontal-accordion'>
     <AccordionItem>
-      <AccordionButton>L<br/>o<br/>r<br/>e<br/>m<br/></AccordionButton>
-      <AccordionContent>Lorem ipsum dolor sit amet. Aut vitae iusto et dolores magnam sit consequuntur dolores. Sit culpa inventore et ipsum nesciunt sed esse maiores.</AccordionContent>
+      <AccordionButton className='accordion-button'>L<br/>o<br/>r<br/>e<br/>m<br/></AccordionButton>
+      <AccordionContent className='accordion-content'>Lorem ipsum dolor sit amet. Aut vitae iusto et dolores magnam sit consequuntur dolores. Sit culpa inventore et ipsum nesciunt sed esse maiores.</AccordionContent>
     </AccordionItem>
+    <AccordionItem>
+      <AccordionButton className='accordion-button'>L<br/>o<br/>r<br/>e<br/>m<br/></AccordionButton>
+      <AccordionContent className='accordion-content'>Lorem ipsum dolor sit amet. Aut vitae iusto et dolores magnam sit consequuntur dolores. Sit culpa inventore et ipsum nesciunt sed esse maiores.</AccordionContent>
+    </AccordionItem>
+    <AccordionItem>
+      <AccordionButton className='accordion-button'>L<br/>o<br/>r<br/>e<br/>m<br/></AccordionButton>
+      <AccordionContent className='accordion-content'>Lorem ipsum dolor sit amet. Aut vitae iusto et dolores magnam sit consequuntur dolores. Sit culpa inventore et ipsum nesciunt sed esse maiores.</AccordionContent>
+    </AccordionItem>
+    <AccordionItem>
+      <AccordionButton className='accordion-button'>L<br/>o<br/>r<br/>e<br/>m<br/></AccordionButton>
+      <AccordionContent className='accordion-content'>Lorem ipsum dolor sit amet. Aut vitae iusto et dolores magnam sit consequuntur dolores. Sit culpa inventore et ipsum nesciunt sed esse maiores.</AccordionContent>
+    </AccordionItem>
+    
     <AccordionItem defaultOpen="true">
-      <AccordionButton>I<br/>p<br/>s<br/>u<br/>m</AccordionButton>
-      <AccordionContent>Est illo aperiam et temporibus modi a voluptatibus quia ut cupiditate illo ut similique consectetur. Et deleniti cumque ut voluptatum quos id distinctio repellat. Ea numquam consequatur ut vitae omnis hic expedita quia ea voluptas reiciendis ea libero nobis quo minima praesentium est neque quas.</AccordionContent>
+      <AccordionButton className='accordion-button'>I<br/>p<br/>s<br/>u<br/>m</AccordionButton>
+      <AccordionContent className='accordion-content'>Est illo aperiam et temporibus modi a voluptatibus quia ut cupiditate illo ut similique consectetur. Et deleniti cumque ut voluptatum quos id distinctio repellat. Ea numquam consequatur ut vitae omnis hic expedita quia ea voluptas reiciendis ea libero nobis quo minima praesentium est neque quas.</AccordionContent>
     </AccordionItem>
   </HorizontalAccordion>
 </div>, document.getElementById('root'))
